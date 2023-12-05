9baaf2718e759cddde608c200dfc9b946fcdff28
Craner <iayfnp@gmail.com>
2023-12-04 23:50:24 -0500

    Starting implementation of MC and TD

--- David Silver Reinforcement Learning/Maze.py
+++ David Silver Reinforcement Learning/Maze.py
@@ -11,12 +11,14 @@ class Maze:
         self.state[2:-2:2,2:-2:2] = np.full([size, size], box)
         self.state[1][1] = self.state[-2][-2] = end
 
+        self.returns = np.zeros([self.shape, self.shape, 4])
+        self.values = np.zeros([self.shape, self.shape])
         self.evaluate()
             
     def print(self, pi = None):
         stringify = self.state.copy()
-        table = {   "[1.0, 0.0, 0.0, 0.0]": "\u257F", "[0.0, 1.0, 0.0, 0.0]": "\u257C", "[0.0, 0.0, 1.0, 0.0]": "\u257D", "[0.0, 0.0, 0.0, 1.0]": "\u257E", 
-                    "[0.5, 0.0, 0.5, 0.0]": "\u2507", "[0.0, 0.5, 0.0, 0.5]": "\u2505", "[0.25, 0.25, 0.25, 0.25]": "\u254B", 
+        table = {   "[1.0, 0.0, 0.0, 0.0]": "\u2191", "[0.0, 1.0, 0.0, 0.0]": "\u2192", "[0.0, 0.0, 1.0, 0.0]": "\u2193", "[0.0, 0.0, 0.0, 1.0]": "\u2190", 
+                    "[0.5, 0.0, 0.5, 0.0]": "\u2195", "[0.0, 0.5, 0.0, 0.5]": "\u2194", "[0.25, 0.25, 0.25, 0.25]": "\u254B", 
                     "[0.5, 0.5, 0.0, 0.0]": "\u2517", "[0.0, 0.5, 0.5, 0.0]": "\u250F", "[0.0, 0.0, 0.5, 0.5]": "\u2513", "[0.5, 0.0, 0.0, 0.5]": "\u251B", 
                     "[0.0, 0.33, 0.33, 0.33]": "\u2533", "[0.33, 0.0, 0.33, 0.33]": "\u252B", "[0.33, 0.33, 0.0, 0.33]": "\u253B", "[0.33, 0.33, 0.33, 0.0]": "\u2523" }
         pi = pi.round(2) if type(pi) == np.ndarray else np.full([self.shape, self.shape, 4], 0.25)
@@ -27,11 +29,15 @@ class Maze:
         print(np.array2string(stringify, separator = '', formatter = {'str_kind': lambda x: x}))
 
     def evaluate(self):
-        self.valuesNew = np.zeros([self.shape, self.shape])
+        self.rewards = np.zeros([self.shape, self.shape])
         for y in range(self.shape):
             for x in range(self.shape):
                 if self.state[y][x] == "\u25A2":
-                    self.valuesNew[y][x] = -10**10
+                    self.rewards[y][x] = -10**3
+                elif self.state[y][x] == "\u229A":
+                    self.rewards[y][x] = 10
+                elif self.state[y][x] == " ":
+                    self.rewards = -1
 
     def scrabble(self, num = None):
         if num == None:
@@ -42,3 +48,48 @@ class Maze:
                 self.state[y][x] = "\u25A2"
                 num -= 1
         self.evaluate()
+        
+# dynamic programming
+# full backups
+# class Maze:
+#     def __init__(self, size = 1):
+#         self.shape = size * 2 + 3
+#         box = "\u25A2"
+#         end = "\u229A"
+
+#         self.state = np.full([self.shape, self.shape], ' ')
+#         self.state[0] = self.state[-1] = self.state[:,0] = self.state[:,-1] = np.full([self.shape], box)
+#         self.state[2:-2:2,2:-2:2] = np.full([size, size], box)
+#         self.state[1][1] = self.state[-2][-2] = end
+
+#         self.evaluate()
+            
+#     def print(self, pi = None):
+#         stringify = self.state.copy()
+#         table = {   "[1.0, 0.0, 0.0, 0.0]": "\u2191", "[0.0, 1.0, 0.0, 0.0]": "\u2192", "[0.0, 0.0, 1.0, 0.0]": "\u2193", "[0.0, 0.0, 0.0, 1.0]": "\u2190", 
+#                     "[0.5, 0.0, 0.5, 0.0]": "\u2195", "[0.0, 0.5, 0.0, 0.5]": "\u2194", "[0.25, 0.25, 0.25, 0.25]": "\u254B", 
+#                     "[0.5, 0.5, 0.0, 0.0]": "\u2517", "[0.0, 0.5, 0.5, 0.0]": "\u250F", "[0.0, 0.0, 0.5, 0.5]": "\u2513", "[0.5, 0.0, 0.0, 0.5]": "\u251B", 
+#                     "[0.0, 0.33, 0.33, 0.33]": "\u2533", "[0.33, 0.0, 0.33, 0.33]": "\u252B", "[0.33, 0.33, 0.0, 0.33]": "\u253B", "[0.33, 0.33, 0.33, 0.0]": "\u2523" }
+#         pi = pi.round(2) if type(pi) == np.ndarray else np.full([self.shape, self.shape, 4], 0.25)
+#         for y in range(1, self.shape - 1):
+#             for x in range(1, self.shape - 1):
+#                 if stringify[y][x] == ' ':
+#                     stringify[y][x] = table[str(pi[y][x].tolist())]
+#         print(np.array2string(stringify, separator = '', formatter = {'str_kind': lambda x: x}))
+
+#     def evaluate(self):
+#         self.valuesNew = np.zeros([self.shape, self.shape])
+#         for y in range(self.shape):
+#             for x in range(self.shape):
+#                 if self.state[y][x] == "\u25A2":
+#                     self.valuesNew[y][x] = -10**3
+
+#     def scrabble(self, num = None):
+#         if num == None:
+#             num = (self.shape - 2)**2 // 10
+#         while num > 0:
+#             x, y = np.random.randint(1, self.shape - 1, 2)
+#             if self.state[y][x] == ' ':
+#                 self.state[y][x] = "\u25A2"
+#                 num -= 1
+#         self.evaluate()
--- David Silver Reinforcement Learning/main.py
+++ David Silver Reinforcement Learning/main.py
@@ -24,21 +24,45 @@ maze.scrabble()
 pi = np.full([maze.shape, maze.shape, 4], 0.25) # the policy chooses actions from north, east, south, west
 epochs = 100
 
-def policy_improvement(y, x):
-        adjacent = maze.valuesOld[[y-1, y, y+1, y], [x, x+1, x, x-1]]
-        actions = np.where(adjacent == adjacent.max())[0]
-        pi[y][x] = np.zeros(4)
-        pi[y][x][actions] = 1 / len(actions)
+# def policy_evaluation(y, x, epsilon):
+#     if random.randint(100) < epsilon:
+#         adjacent = maze.returns[y][x]
+#         actions = np.where(adjacent == adjacent.max())[0]
+#     else:
+#         adjacent = maze.rewards[[y-1, y, y+1, y], [x, x+1, x, x-1]]
+#         actions = [elem for elem in adjacent if elem > -10**3]
+#     return actions[random.randint(len(actions))]
 
+# def policy_improvement(y, x):
+#     adjacent = maze.returns[y, x]
+#     actions = np.where(adjacent == adjacent.max())[0]
+#     pi[y][x] = np.zeros(4)
+#     pi[y][x][actions] = 1 / len(actions)
+        
+def policy_evaluation(y, x, epsilon):
+    if random.randint(100) < epsilon:
+        adjacent = maze.values[[y-1, y, y+1, y], [x, x+1, x, x-1]]
+        actions = np.where(adjacent == adjacent.max())[0]
+    else:
+        adjacent = maze.rewards[[y-1, y, y+1, y], [x, x+1, x, x-1]]
+        actions = [elem for elem in adjacent if elem > -10**3]
+    return actions[random.randint(len(actions))]
+
+def policy_improvement(y, x):
+    adjacent = maze.values[[y-1, y, y+1, y], [x, x+1, x, x-1]]
+    actions = np.where(adjacent == adjacent.max())[0]
+    pi[y][x] = np.zeros(4)
+    pi[y][x][actions] = 1 / len(actions)
+    
 # dynamic programming
-#k = 1 # how often to update the policy from the values. value iteration: k = 1, policy iteration: k > 1
-#isSynchronous = True
-#for i in range(epochs):
+# k = 1 # how often to update the policy from the values. value iteration: k = 1, policy iteration: k > 1
+# isSynchronous = True
+# for i in range(epochs):
 #    maze.valuesOld = maze.valuesNew.copy()
 #    if i % k == 0:
-        #for y in range(1, maze.shape - 1):
-        #    for x in range(1, maze.shape - 1):
-        #        policy_improvement(y, x)
+#         for y in range(1, maze.shape - 1):
+#            for x in range(1, maze.shape - 1):
+#                policy_improvement(y, x)
 #    for y in range(1, maze.shape - 1):
 #        for x in range(1, maze.shape - 1):
 #            if not isSynchronous:
@@ -47,49 +71,35 @@ def policy_improvement(y, x):
 #                actions = pi[y][x]
 #                maze.valuesNew[y][x] = sum(actions * maze.valuesOld[[y-1, y, y+1, y], [x, x+1, x, x-1]]) - 1
 
-# forward view eligibility traces
+# td(lambda)
 step = 1 # number of steps to propogate the error. monte-carlo: step = infinity, td: step = 1
 alpha = 0.001 # learning rate. how well the algorithm remembers past episodes
-epsilon = 95
 gamma = 0.99
 y = x = 0
-maze.valuesOld = maze.valuesNew.copy
-rewards = np.full([maze.shape, maze.shape], -1)
-rewards[1][1] = rewards[-2][-2] = 10
 for i in range(epochs):
     # initialize the episode
-    policy_improvement()
     while maze.state[y][x] != ' ':
         y, x = np.random.randint(1, maze.shape - 1, 2)
 
     history = deque()
+    epsilon = i / epochs
     while maze.state[y][x] != "\u229A":
-        if random.randint(100) < epsilon:
-            adjacent = maze.valuesNew[[y-1, y, y+1, y], [x, x+1, x, x-1]]
-            actions = np.where(adjacent == adjacent.max())[0]
-            policy_improvement(y, x)
-            action = actions[random.randint(len(actions))]
-        else:
-            print()
+        history.append((y, x))
+        if len(history) == step:
+            reward = maze.values[y][x]
+            for y, x in history[::-1]:
+                reward = maze.rewards[y][x] + gamma * reward
+            maze.values[y][x] = maze.values[y][x] + alpha * (reward - maze.values[y][x])
+            history.popleft()
 
+        action = policy_evaluation(epsilon)
         y += 0 if action % 2 == 0 else action - 1
         x += 0 if action % 2 == 1 else 2 - action
-        history.append(reward)
-        if len(history) == step or (len(history) < step and maze.state[y][x] == "\u229A"):
-            reward = maze.valuesNew[y][x]
-            for y, x in history:
-                reward += -1
-                maze.valuesNew[y][x] = maze.valuesNew[y][x] + alpha * (reward - maze.valuesNew[y][x])
-        else:
-            y, x = history[-step]
-            reward = maze.valuesNew[y][x] - step
-            maze.valuesNew[y][x][action] = maze.valuesNew[y][x] + alpha * (reward - maze.valuesNew[y][x])
-
-## td(lambda)
-#lamb = 1 # number of steps to propogate the error. monte-carlo: step = infinity, td: step = 1
-#alpha = 0.001 # learning rate. how long the algorithm will remember past episodes
-#y = x = 0
-
+    reward = maze.values[y][x]
+    for y, x in history[::-1]:
+        reward = maze.rewards[y][x] + gamma * reward
+        maze.values[y][x] = maze.values[y][x] + alpha * (reward - maze.values[y][x])
+             
 # next learning mumbo jumbo
 
 maze.print(pi)
